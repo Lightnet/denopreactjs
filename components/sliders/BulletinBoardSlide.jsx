291177cf97a6f8e1d4733f53e1c989866b3a72a2
Lightnet <darkneter@gmail.com>
2022-08-07 11:43:25 -0700

    Testing slide.

--- components/sliders/BulletinBoardSlide.jsx
+++ components/sliders/BulletinBoardSlide.jsx
@@ -4,6 +4,14 @@
   Created By: Lightnet
 */
 
+/*
+
+Left Slide in and out
+Right Slide in and out
+
+
+*/
+
 /** @jsxRuntime classic */
 /** @jsx h */
 import { h } from "preact"
@@ -13,12 +21,23 @@ import { useState } from "preact/compat";
 export default function BulletinBoardSlide() {
   // State for the counter
   const [count, setCount] = useState(0);
-  function add(){
+  const [bSlide, setBSlide] = useState("sideBoard_ROut");
+  function toggle(){
     console.log("add")
     setCount(state=>state+1)
+    if(bSlide=="sideBoard_ROut"){
+      setBSlide("slideBoard_RIn")
+    }else{
+      setBSlide("sideBoard_ROut")
+    }
   }
 
   return (<div>
-    <label>BulletinBoardSlide: {count}</label>
+    <div style="width:600px;height:200px;background:gray;overflow: clip;">
+      <div class={bSlide} style="width:600px;height:200px;background:blue;">
+
+      </div>
+    </div>
+    <label onClick={toggle}>BulletinBoardSlide</label>
   </div>)
 }
